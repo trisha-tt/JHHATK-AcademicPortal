5d15023a0a41248431595410dbead248d7e81369
trisha <trisha@teredesai.com>
2024-05-02 19:25:54 -0700

    pseudocode

--- Admin.h
+++ Admin.h
@@ -5,41 +5,35 @@
 
 class Admin
 {
-    private:
+private:
     std::string name;
     LoginDatabase email;
 
-    public:
+public:
     void login();
     void addTutor();
     void viewReports();
-
 };
-
 
 void Admin::login()
 // user can login as admin in order to have permissions to do the functions below (that TAs/students can’t do)
 {
-
-
-
 }
-
 
 void Admin::addTutor()
 // admin can add a new tutor to the login database/system
 {
-
-
-
+    // Get Tutor Login info from user (Admin - Manager of MLC).
+    // Check if the Tutor Login Info is already in the LoginDatabase
+    // If not, add Tutor Login Info to the database
+    // if it is, give an error message to the User
 }
 
 void Admin::viewReports()
 // displays all of the reports generated/requested by the admin, showing statistics/information on student queries (information depending on the report type)
 // uses functions from class Report
 {
-
-
-
-
+    // Use function getReportType to receive the input from user (Admin) on desired report type
+    // According to their input, the appropriate function is called to generate the desired report
 }
+
--- RequestInterface.h
+++ RequestInterface.h
@@ -13,38 +13,31 @@ public:
     int countRequests();
 };
 
-
-bool RequestInterface::acceptFlags() 
-//allows the TA to assign the request to themselves & indicate that they will help the student
+bool RequestInterface::acceptFlags()
+// allows the TA to assign the request to themselves & indicate that they will help the student
 {
-
-
-
+    // The Tutor will be able to view all the active requests made by the students in the MLC
+    // When the tutor accepts the request, it will be assigned to them, hence removing it from the active queue
 }
-
 
 bool RequestInterface::deleteFlags()
-//allows the TA to delete any requests that are unneeded
+// allows the TA to delete any requests that are unneeded
 {
-
-
-
+    // Allows the Tutor to delete the request as needed
+    // Examples of usage: duplicate request, student unavailable, etc
+    // Doing so removes the request from the active queue and the Tutor's personal queue
 }
-
 
 bool RequestInterface::flagComplete()
 // allows the TA to mark a request as done
 {
-
-
-
+    // Allows the Tutor to mark the request as completed after assisting the student
+    // If there is any incomplete information in the request, the Tutor will be able to fill the necessary details
+    // The Tutor must also include a resolution message, describing how the issue was solved
 }
 
-
-int countRequests()
-//counts the number of completed requests
-// used by class Reports when creating reports
+int RequestInterface::countRequests()
+// counts the number of completed requests
+//  used by class Reports when creating reports
 {
-
-
 }
